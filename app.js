731d028e7e1d8946622de27e5ea7affbe10aaf50
quinn <quinn.kennedy@stonybrook.edu>
2019-04-11 14:23:08 -0400

    asd

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var connection = mysql.createConnection({
     database: 'hw7'
 });
 var app = express();
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 80;
 connection.connect();
@@ -20,57 +20,55 @@ const getQuery = 'SELECT * FROM assists WHERE club=? AND POS =?;';
 let memcached = new Memcached("127.0.0.1:11211")
 
 app.get('/hw7', function (req, res) {
-//    console.log('GET on hw7 with query = ' + req.query);
-//    console.log('club = ' + req.query.club);
-//    console.log('pos = ' + req.query.pos);
+    //    console.log('GET on hw7 with query = ' + req.query);
+    //    console.log('club = ' + req.query.club);
+    //    console.log('pos = ' + req.query.pos);
 
-	const queryJSON = JSON.stringify(req.query);
+    const queryJSON = JSON.stringify(req.query);
 
-	memcached.get(queryJSON, function (err, data) {
-	  if(data){
-//		  console.log(data);
-		return res.json(data);
-	  }else{
+    memcached.get(queryJSON, function (err, data) {
+        if (data) {
+            // console.log(data);
+            return res.json(data);
+        } else {
             console.log('not in mc');
-	    connection.query(getQuery, [req.query.club, req.query.pos], function (err, results, fields) {
-	//        console.log('err = ' + err);
-	//        console.log('results = ' + results);
-	//        console.log('fields = ' + fields);
+            connection.query(getQuery, [req.query.club, req.query.pos], function (err, results, fields) {
+                //        console.log('err = ' + err);
+                //        console.log('results = ' + results);
+                //        console.log('fields = ' + fields);
+                let topPlayer = results[0];
+                let avg = 0;
 
+                results.forEach(element => {
+                    if (element.A > topPlayer.A) {
+                        topPlayer = element;
+                        //	console.log('new topPlayer = ' + topPlayer.Player);
+                    } else if (element.A == topPlayer.A) {
+                        topPlayer = element.GS > topPlayer.GS ? element : topPlayer;
+                        //		console.log('new topPlayer = ' + topPlayer.Player);
+                    }
 
-		let topPlayer = results[0];
-		let avg = 0;
+                    avg += element.A;
+                    //	    console.log('new Avg = ' + avg);
+                });
 
-		results.forEach(element => {
-		    if (element.A > topPlayer.A) {
-			topPlayer = element;
-		//	console.log('new topPlayer = ' + topPlayer.Player);
-		    } else if (element.A == topPlayer.A) {
-			topPlayer = element.GS > topPlayer.GS ? element : topPlayer;
-	//		console.log('new topPlayer = ' + topPlayer.Player);
-		    }
+                avg /= results.length;
 
-		    avg += element.A;
-	//	    console.log('new Avg = ' + avg);
-		});
+                const jsonResult = {
+                    club: req.query.club,
+                    max_assists: topPlayer.A,
+                    player: topPlayer.Player,
+                    avg_assists: avg
+                };
 
-		avg /= results.length;
-
-		const jsonResult = {
-		    club: req.query.club,
-		    max_assists: topPlayer.A,
-		    player: topPlayer.Player,
-		    avg_assists: avg
-		};
-
-		// const str_json = JSON.stringify(jsonResult);
-		memcached.set(queryJSON, jsonResult, 500, function (err) {
-		//console.log('err in set memc ' + err) 
-		});
-		res.json(jsonResult);
-	    });
-	  }
-	});
+                // const str_json = JSON.stringify(jsonResult);
+                memcached.set(queryJSON, jsonResult, 500, function (err) {
+                    //console.log('err in set memc ' + err) 
+                });
+                res.json(jsonResult);
+            });
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
