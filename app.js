119f9944299c549286d0b9aafcfd8da6b5aef4b8
quinn <quinn.kennedy@stonybrook.edu>
2019-04-10 22:41:55 -0400

    test on cloud

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var mysql = require('mysql')
+const express = require('express');
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '3123',
     database: 'hw7'
 });
+app.use(bodyParser.json()); // for parsing application/json
 
-connection.connect()
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+connection.connect();
 
 connection.query('select * from assists', function (err, rows, fields) {
     if (err) throw err
@@ -14,6 +19,22 @@ connection.query('select * from assists', function (err, rows, fields) {
     rows.array.forEach(element => {
         console.log(element);
     });
-})
+});
+const getQuery = 'SELECT club, pos, max_assists, player, avg_assists 
+FROM assists
+WHERE a =?, pos =? ';
+app.get('/hw7', function (req, res) {
+    console.log('GET on hw7 with query = ' + req.query);
+    console.log('club = ' + req.query.club);
+    console.log('pos = ' + req.query.pos);
+    connection.query(getQuery, [req.query.club, req.query.pos], function (err, results, fields) {
+        console.log('err = ' + err);
+        console.log('results = ' + results);
 
-connection.end()
+        console.log('fields = ' + fields);
+    });
+});
+
+// connection.end();
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
