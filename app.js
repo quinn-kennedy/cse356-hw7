354677b47d5967d829df99e5911d850f118023d7
Ubuntu <ubuntu@elastic.cloud.compas.cs.stonybrook.edu>
2019-04-11 18:21:18 +0000

    format

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var Memcached = require('memcached');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -15,44 +16,61 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 const port = 80;
 connection.connect();
 
-const getQuery = 'SELECT club, pos, A, player FROM assists WHERE A=? AND POS =?;';
+const getQuery = 'SELECT * FROM assists WHERE club=? AND POS =?;';
+let memcached = new Memcached("127.0.0.1:11211")
+
 app.get('/hw7', function (req, res) {
-    console.log('GET on hw7 with query = ' + req.query);
-    console.log('club = ' + req.query.club);
-    console.log('pos = ' + req.query.pos);
-    connection.query(getQuery, [req.query.club, req.query.pos], function (err, results, fields) {
-        console.log('err = ' + err);
-        console.log('results = ' + results);
-        console.log('fields = ' + fields);
+//    console.log('GET on hw7 with query = ' + req.query);
+//    console.log('club = ' + req.query.club);
+//    console.log('pos = ' + req.query.pos);
+
+	const queryJSON = JSON.stringify(req.query);
+
+	memcached.get(queryJSON, function (err, data) {
+	  if(data){
+//		  console.log(data);
+		return res.json(data);
+	  }else{
+            console.log('not in mc');
+	    connection.query(getQuery, [req.query.club, req.query.pos], function (err, results, fields) {
+	//        console.log('err = ' + err);
+	//        console.log('results = ' + results);
+	//        console.log('fields = ' + fields);
 
 
-        const topPlayer = results[0];
-        const avg = 0;
+		let topPlayer = results[0];
+		let avg = 0;
 
-        results.forEach(element => {
-            if (element.A > topPlayer.A) {
-                topPlayer = element;
-            } else if (element.A == topPlayer.A) {
-                topPlayer = element.GS > topPlayer.GS ? element : topPlayer;
-            }
+		results.forEach(element => {
+		    if (element.A > topPlayer.A) {
+			topPlayer = element;
+		//	console.log('new topPlayer = ' + topPlayer.Player);
+		    } else if (element.A == topPlayer.A) {
+			topPlayer = element.GS > topPlayer.GS ? element : topPlayer;
+	//		console.log('new topPlayer = ' + topPlayer.Player);
+		    }
 
-            avg += element.A;
-        });
+		    avg += element.A;
+	//	    console.log('new Avg = ' + avg);
+		});
 
-        avg /= results.length;
+		avg /= results.length;
 
-        const jsonResult = {
-            club: topPlayer.club,
-            max_assists: topPlayer.A,
-            player: topPlayer.Player,
-            avg_assists: avg
-        };
+		const jsonResult = {
+		    club: req.query.club,
+		    max_assists: topPlayer.A,
+		    player: topPlayer.Player,
+		    avg_assists: avg
+		};
 
-
-        res.json(jsonResult);
-    });
+		// const str_json = JSON.stringify(jsonResult);
+		memcached.set(queryJSON, jsonResult, 500, function (err) {
+		//console.log('err in set memc ' + err) 
+		});
+		res.json(jsonResult);
+	    });
+	  }
+	});
 });
-
-// connection.end();
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
